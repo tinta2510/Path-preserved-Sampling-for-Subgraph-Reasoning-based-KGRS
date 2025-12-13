0d31f50837943add605b5d333998819edbf7d18e
tinta2510 <tatrungtin2004@gmail.com>
2025-12-13 23:36:55 +0700

    small chages for multiple GPUs

--- base_model.py
+++ base_model.py
@@ -16,7 +16,7 @@ class BaseModel(object):
          
         if torch.cuda.device_count() > 1:
             print("Using", torch.cuda.device_count(), "GPUs!")
-            self.model = torch.nn.DataParallel(self.model)
+            self.model = torch.nn.DataParallel(self.model, device_ids=list(range(torch.cuda.device_count())))
          
         self.model.to(self.device)
 
@@ -60,8 +60,6 @@ class BaseModel(object):
             subs, rels, pos, neg = self.loader.get_batch(batch_idx)
 
             self.optimizer.zero_grad()
-            subs = torch.as_tensor(subs, dtype=torch.long, device=self.device)
-            rels = torch.as_tensor(rels, dtype=torch.long, device=self.device)
             scores = self.model(subs, rels) 
            
             loss = cal_bpr_loss(self.n_users, pos, neg, scores)
@@ -135,8 +133,6 @@ class BaseModel(object):
             end = min(n_data, (id+1)*batch_size)
             batch_idx = np.arange(start, end)
             subs, rels, objs = self.loader.get_batch(batch_idx, data='test')
-            subs = torch.as_tensor(subs, dtype=torch.long, device=self.device)
-            rels = torch.as_tensor(rels, dtype=torch.long, device=self.device)
             scores = self.model(subs, rels, mode='test').data.cpu().numpy()
         
             batch_recall, batch_ndcg = 0, 0
