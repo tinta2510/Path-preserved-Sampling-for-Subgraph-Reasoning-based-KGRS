c4c09ed536d2ad7c3955d5b8718769cdd73ad4dd
tinta2510 <tatrungtin2004@gmail.com>
2025-12-20 18:09:01 +0700

    Used lookup for get_neighbours

--- models/subgraph_model.py
+++ models/subgraph_model.py
@@ -352,10 +352,12 @@ class AdaptiveSubgraphModel(torch.nn.Module):
 
         for i in range(self.n_layer):
             # Expand user-centric computation graph for this layer
+            t0 = time.time()
             nodes_np = nodes.detach().cpu().numpy()
             with threadpool_limits(limits=16):
                 nodes, edges, old_nodes_new_idx = self.loader.get_neighbors(nodes_np, mode=mode)
-
+            print(f"Get neighbors time (layer {i}): {time.time() - t0:.4f} sec before size {nodes_np.shape[0]} after size {nodes.shape[0]} edges {edges.shape[0]}")
+            
             nodes = nodes.to(self.device, non_blocking=True)
             edges = edges.to(self.device, non_blocking=True)
             old_nodes_new_idx = old_nodes_new_idx.to(self.device, non_blocking=True)
